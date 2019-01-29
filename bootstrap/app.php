9b41e6b85954a0b6b8b2959b7ef6bdc95b37faca
郑懿彬 <lance.zyb@gmail.com>
2019-01-29 16:36:00 +0800

    laravel在bootstrap/app.php做了啥

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,4 +6,10 @@
  * Time: 4:23 PM
  */
 
-echo('I\'m bootstrap/app.php.');
+/*
+ * Laravel 框架的 bootstrap/app.php
+ * 1. 实例化一个容器对象（Illuminate\Foundation\Application）$app
+ * 2. 在$app分别注册了3个单例对象：Http\Kernel，Console\Kernel和Debug\ExceptionHandler
+ * 3. return $app
+ */
+
